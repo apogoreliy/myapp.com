9b5cbcad7aea8920a286e3b5bcb337c888a95cd3
apogoreliy <apogoreliy@gmail.com>
2016-12-08 22:22:48 +0200

    Change app's structure

---
+++ app/components/Category.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Category = () => (
+    <div style={{marginTop: "10px"}}>
+        <span className="btn btn-default" style={{marginRight: "10px"}}>X</span>
+        <span>Телефоны</span>
+    </div>
+);
+
+export default Category;
---
+++ app/components/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Product = () => (
+    <tr>
+        <td>1</td>
+        <td>HTC</td>
+        <td>20000</td>
+        <td>25000</td>
+    </tr>
+);
+
+export default Product;
