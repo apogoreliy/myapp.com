4c483d56e71f09b0f9e30d5978ad2a4139437a09
apogoreliy <apogoreliy@gmail.com>
2016-12-11 20:27:16 +0200

    Handling routs on server correct

--- server/router.js
+++ server/router.js
@@ -2,47 +2,58 @@ const DB = require('./db');
 
 module.exports = (app) =>{
     app.post('/add_category', (req, res)=> {
-        DB.addCategory(req.body);
-    });
-
-    app.post('/add_product', (req, res)=> {
-        DB.addProduct(req.body);
-    });
-
-    app.post('/edit_product', (req, res)=> {
-        DB.editProduct(req.body);
-    });
-
-    app.post('/remove_product', (req, res)=> {
-        DB.removeProduct(req.body);
-    });
-
-    app.post('/remove_category', (req, res) =>{
-        DB.removeCategory(req.body);
-    });
-
-    app.post('/get_products', (req, res) => {
-        DB.getProducts(function(r){
-            res.json(r);
-        });
-    });
-
-    app.post('/get_categories', (req, res) => {
-        /*
-        DB.getCategories(function(r){
-            console.log('rrrrrrrrrrrrr', r);
-            res.json(r);
-        });
-        */
-
-        //console.info('The promise was fulfilled with items!', DB.getCategories());
-
-        DB.getCategories().then(function(items) {
-            console.info('The promise was fulfilled with items!', items);
+        DB.addCategory(req.body).then(function(items) {
             res.json(items);
         }, function(err) {
             console.error('The promise was rejected', err, err.stack);
         });
+    });
 
+    app.post('/add_product', (req, res)=> {
+        DB.addProduct(req.body).then(function(items) {
+            res.json(items);
+        }, function(err) {
+            console.error('The promise was rejected', err, err.stack);
+        });
+    });
+
+    app.post('/edit_product', (req, res)=> {
+        DB.editProduct(req.body).then(function(items) {
+            res.json(items);
+        }, function(err) {
+            console.error('The promise was rejected', err, err.stack);
+        });
+    });
+
+    app.post('/remove_product', (req, res)=> {
+        DB.removeProduct(req.body).then(function(items) {
+            res.json(items);
+        }, function(err) {
+            console.error('The promise was rejected', err, err.stack);
+        });
+    });
+
+    app.post('/remove_category', (req, res) =>{
+        DB.removeCategory(req.body).then(function(items) {
+            res.json(items);
+        }, function(err) {
+            console.error('The promise was rejected', err, err.stack);
+        });
+    });
+
+    app.post('/get_products', (req, res) => {
+        DB.getProducts().then(function(items) {
+            res.json(items);
+        }, function(err) {
+            console.error('The promise was rejected', err, err.stack);
+        });
+    });
+
+    app.post('/get_categories', (req, res) => {
+        DB.getCategories().then(function(items) {
+            res.json(items);
+        }, function(err) {
+            console.error('The promise was rejected', err, err.stack);
+        });
     });
 };
--- server/server.js
+++ server/server.js
@@ -2,23 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const router = require('./router');
-
-const controller = require('./controllers/controllers');
-
-const opts = {
-    server: {
-        socketOptions: { keepAlive: 1 }
-    }
-};
-//mongoose.connect('mongodb://apogoreliy:7spirits@ds131878.mlab.com:31878/myapp', opts);
 
 // App Setup
 app.use(express.static('public'));
 app.use(bodyParser.json({ type: '*/*' }));
-
-//controller.registerRoutes(app);
 router(app);
 
 // Server Setup
